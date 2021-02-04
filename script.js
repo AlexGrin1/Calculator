69dd9ada54c1e0066423582a4a6c4773185a2663
AlexGrin1 <a_grynevich@mail.ru>
2021-02-04 21:26:37 +0300

    refactor: displaying numbers on the screen

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const buttons = document.querySelector(".calc");
+const screen = document.querySelector(".scr");
+console.log(buttons);
+buttons.addEventListener("click", (event) => {
+  screen.innerHTML = event.target.innerHTML;
 
+  console.log(event.target.innerHTML);
+});
